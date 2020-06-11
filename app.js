b5438f3725bb7d37089fd9108f7cbb04b989c62e
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-11 16:40:25 -0700

    added success and failure page, as well as made paths for them in app.js

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
-})
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.get("/success", function(req, res){
+    res.sendFile(__dirname + "/success.html");
+});
+
+app.get("/failure", function (req, res){
+    res.sendFile(__dirname + "/failure.html");
+});
 
 app.listen(3000, function(){
     console.log("App listening on PORT 3000");
-})
+});
