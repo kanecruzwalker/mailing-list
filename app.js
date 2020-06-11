7d222e2ca36aaf3815083b0d78281986e8a844f2
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-11 16:28:10 -0700

    installed body-parseras a dependency, implemented it in app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 })
