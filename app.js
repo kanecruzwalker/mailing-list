816d4ff3363008466982e6a8836d76ec45f04cea
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-11 19:38:14 -0700

    started with making post request to mailChimp server using https methods

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+require("dotenv").config();
+
+
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,12 +14,41 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
 app.post("/", function (req, res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.uEmail;
-    console.log(firstName);
-    console.log(lastName);
-    console.log(email);
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.uEmail;
+
+
+
+    var data = {
+        members:[
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+
+                }
+            }
+        ]
+    }
+    const jsonData = JSON.stringify(data);
+
+    const url = "https:us10.api.mailchimp.com/3.0/lists/"+process.env.LIST;
+    const options = {
+        method: "POST",
+        auth: "kane:"+process.env.MAIL_CHIMP
+    }
+
+    const request =https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
 });
 
 app.get("/success", function(req, res){
