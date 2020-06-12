dfdb72f2e1ca09bdcad42e73dee96b0267f0febc
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-11 17:24:58 -0700

    used express.static to make a folder that can be accessed through relative path, to allow us to access staic html and image pages in the document

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
