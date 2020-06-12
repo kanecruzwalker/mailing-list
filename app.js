45914a075759d972e91b83f578e956732326fbf3
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-11 20:43:56 -0700

    made failure page and success page implement boot strap, made them render pending on user's success signing up

--- app.js
+++ app.js
@@ -42,23 +42,27 @@ app.post("/", function (req, res){
     }
 
     const request =https.request(url, options, function(response){
+        
+        if (response.statusCode === 200 ){
+            res.sendFile(__dirname + "/success.html");
+        }else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        
         response.on("data", function(data){
             console.log(JSON.parse(data));
-        })
+        });
     });
 
     request.write(jsonData);
     request.end();
 });
 
-app.get("/success", function(req, res){
-    res.sendFile(__dirname + "/success.html");
+app.post("/failure", function(req, res){
+    res.redirect("/");
 });
 
-app.get("/failure", function (req, res){
-    res.sendFile(__dirname + "/failure.html");
-});
 
-app.listen(3000, function(){
+app.listen(process.env.PORT ||3000, function(){
     console.log("App listening on PORT 3000");
 });
