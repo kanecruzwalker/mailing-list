d1515f5039a8f5bb338cd0579f8719e5fb32f1fb
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-11 17:53:32 -0700

    added name attributes to inputs, added post route to app.js, logging user's inputs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const https = require("https");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -8,6 +9,14 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function (req, res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.uEmail;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
 });
 
 app.get("/success", function(req, res){
